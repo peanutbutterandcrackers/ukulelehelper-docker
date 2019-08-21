d271c304cd650f9b9444c9dc19022cb75ec2c26c
Prafulla Giri <pratheblackdiamond@gmail.com>
2019-08-21 06:19:56 +0545

    Update setup.sh: add logic to prevent re-entry of the custom command
    This prevents ~/.bashrc from being cluttered up on multiple runs of
    setup.sh

--- setup.sh
+++ setup.sh
@@ -1,4 +1,9 @@
-#!/bin/bash
+#!/bin/bash -i
+########################
+# -i (interactive) mode because bash scripts, apparently,
+# don't have access to shell functions or aliases; and it
+# is necessary to check for the existance of one so as to
+# not clutter up the .bashrc file on multipe executions.
 
 container_port=8000
 host_port=80
@@ -6,5 +11,16 @@ host_port=80
 docker build --tag ukehelper .
 docker run --detach --name uke_helper --publish $host_port:$container_port ukehelper
 
-# INSTEAD OF THIS, REGISTER A FUNCTION THAT CAN START, STOP THE CONTAINER
-echo -e "\nalias ukehelp='docker start uke_helper'" >> ~/.bashrc
+# If the shell function that we add to .bashrc already exists, don't do anything
+command -v ukehelp || {
+cat << _EOF_
+
+function ukehelp {
+	if [ "\$1" == 'stop' ]; then
+		docker stop uke_helper
+	else
+		docker start uke_helper
+	fi
+}
+_EOF_
+} >> ~/.bashrc
